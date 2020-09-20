37649ea3a05aae609c61feb9674b847980abe87c
JuanBonelli <67331365+JuanBonelli@users.noreply.github.com>
2020-09-20 18:54:09 -0300

    Tarea terminada

--- 2_factorial.asm
+++ 2_factorial.asm
@@ -21,14 +21,14 @@ addi $a0, $t0, -1
 
 loop:
 beq $a0, $zero, exit
-mul $t0, $t0, $a0     #multiplico y guardo en t0
-addi $a0, $a0, -1     #le resto 1 al numero
+mul $t0, $t0, $a0      #multiplico y guardo en t0
+addi $a0, $a0, -1      #le resto 1 al numero
 
-j loop                #hacemos que itere el ciclo
+j loop                 #hacemos que itere el ciclo
 
 exit:
 li $v0, 1
-move $a0, $t0         #muevo lo que esta en t0 a a0
+move $a0, $t0          #muevo lo que esta en t0 a a0
 syscall
 
 #exit
